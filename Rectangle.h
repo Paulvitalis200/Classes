1df66a92119ee6d43db19d0957a3eaf0880be2bd
Paul Otieno <vitalispaul48@live.com>
2025-12-24 13:44:29 +0300

    Define a class

---
+++ Rectangle.h
@@ -0,0 +1,17 @@
+//
+// Created by Admin on 24/12/2025.
+//
+
+#ifndef CLASSES_RECTANGLE_H
+#define CLASSES_RECTANGLE_H
+
+
+class Rectangle {
+    int width;
+    int height;
+    void draw();
+    int getArea();
+};
+
+
+#endif //CLASSES_RECTANGLE_H
