6e759a568723b763bc103212684f7ea50b6fc118
Paul Otieno <vitalispaul48@live.com>
2026-03-20 05:58:37 +0300

    Templates intro

--- main.cpp
+++ main.cpp
@@ -1,21 +1,17 @@
 #include <iostream>
-#include "FileIO.h"
-#include "Rectangle.h"
-#include "AccountLocked.h"
 
 using namespace std;
 
-void login() {
-    throw AccountLocked{};
+template<typename T>
+T larger(T first, T second) {
+    return (first > second) ? first : second;
 }
-int main() {
-    // Account Locked
-    try {
-        login();
-    }
-    catch (const exception& ex) {
-        cout << ex.what();
-    }
 
+int main() {
+
+    larger(1.1, 2.2);
+    // larger(1, 2);
+    larger(1.2, 3.3);
+    // larger("1", "3");
     return 0;
 }
