1a6746b1e16e21186e6163f9bc49436a42558f0a
Paul Otieno <vitalispaul48@live.com>
2026-03-20 06:04:41 +0300

    Templates with multiple parameters

--- main.cpp
+++ main.cpp
@@ -7,9 +7,13 @@ T larger(T first, T second) {
     return (first > second) ? first : second;
 }
 
+template<typename K, typename V>
+void display(K key, V value) {
+    cout << key << "=" << value << endl;
+}
+
 int main() {
-
-    auto result = larger<double>(1, 3.3);
-
+    display("a", 1);
+    display(1, 1);
     return 0;
 }
