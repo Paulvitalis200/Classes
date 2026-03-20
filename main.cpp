f6ffdf13902195cde53e6642d38968962031f8f9
Paul Otieno <vitalispaul48@live.com>
2026-03-20 06:01:12 +0300

    Explicit type arguments

--- main.cpp
+++ main.cpp
@@ -9,9 +9,7 @@ T larger(T first, T second) {
 
 int main() {
 
-    larger(1.1, 2.2);
-    // larger(1, 2);
-    larger(1.2, 3.3);
-    // larger("1", "3");
+    auto result = larger<double>(1, 3.3);
+
     return 0;
 }
